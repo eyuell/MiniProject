326f3f6f314540fbbbf9c9e04de757dad100dd94
eyuell <45358912+eyuell@users.noreply.github.com>
2018-12-09 21:56:05 +0100

    Encapsulation and Methods

--- Finance.java
+++ Finance.java
@@ -2,56 +2,39 @@
 public class Finance {//Super
     private double budget;
     private double earnedValue;
-double projectDuration;
-double daysPassed;
-String projectID;
+    private String projectId;
+    private double projectDuration;
+    private double daysPassed;
 
-    public Finance(double budget,double earnedValue,double projectDuration,double daysPassed,String projectID) {
+    public Finance(double budget, double earnedValue, double projectDuration, double daysPassed, String projectId) {
         this.budget = budget;
         this.earnedValue = earnedValue;
         this.daysPassed = daysPassed;
-    this.projectDuration=projectDuration;
-    this.projectID=projectID;
+        this.projectDuration = projectDuration;
+        this.projectId = projectId;
     }
 
     public double getBudget() {
         return this.budget;
     }
 
-    public void setBudget(double budget) {
-        this.budget = budget;
-    }
-
     public double getEarnedValue() {
         return earnedValue;
     }
 
-    public void setEarnedValue(double earnedValue) {
-        this.earnedValue = earnedValue;
+    public String getProjectId() {
+        return projectId;
     }
-
 
     public double getDaysPassed() {
         return daysPassed;
-    }
-
-    public void setDaysPassed(double daysPassed) {
-        this.daysPassed = daysPassed;
     }
 
     public double getProjectDuration() {
         return projectDuration;
     }
 
-    public void setProjectDuration(double projectDuration) {
-        this.projectDuration = projectDuration;
-    }
-
-    public String getProjectID() {
-        return projectID;
-    }
-
-    public void setProjectID(String projectID) {
-        this.projectID = projectID;
+    public void setEarnedValue(double earnedValue) {
+        this.earnedValue = earnedValue;
     }
 }
