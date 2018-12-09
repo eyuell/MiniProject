88313e050bd590e7829191d9080b4e415beb2b8c
armin2118 <43929673+armin2118@users.noreply.github.com>
2018-12-09 19:01:29 +0100

    Update Main.java
    Main to try SV CV on

--- Main.java
+++ Main.java
@@ -1,23 +1,15 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-
 public class Main {
-    public static void main(String[]args){
-
-      systemStore drake=new systemStore();
-        drake.registerScheduleVariance(100000, 50000,38 ,12,1 );
-        drake.registerScheduleVariance(200000, 50000,38 ,15,2 );
-        drake.registerCostVariance(100000,50000 ,38 ,12 , 78000 );
-        System.out.println(drake.sizeOfArray());
+    public static void main(String[] args) {
+        systemStore drake = new systemStore();
+        drake.registerScheduleVariance(100000, 50000, 38, 12, "1");
+        drake.registerScheduleVariance(200000, 50000, 38, 15, "2");
+        drake.registerCostVariance(100000, 50000, 38, 12, 78000,"1");
+        //System.out.println(drake.sizeOfArray());
+        //System.out.println(drake.printAllFinances());
+        System.out.println( drake.printAllFinancesByID("1"));
         System.out.println(drake.printAllFinances());
     }
+
 }
 
 
-/*TODO: different classes move method to System. Try printing in main. Methods are done. Learn to add them to array,
-TODO: Start on JsonFile.
-TODO: Understand the graphical UI
-TODO: Go to georges GIT and read the file where he used GUI and JSON files.
-TODO: make the code run by 12/10.2018. WITHOUT GUI. METHODS SHOULD STORE AND BE FUNCTIONAL.
-TODO: WORK ON GUI WHEN TIME COMES. STUDY QT OR FMXL.
- */
