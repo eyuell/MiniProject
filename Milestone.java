508d71d6ca67e68e1f4d94a26d946224f2bad666
eyuell <45358912+eyuell@users.noreply.github.com>
2018-12-09 22:00:55 +0100

    Milestones Class

---
+++ Milestone.java
@@ -0,0 +1,39 @@
+package MiniProject;
+
+import java.time.LocalDate;
+
+public class Milestone {
+    private String name;
+    private String id;
+    private LocalDate date;
+
+    public Milestone (String name, String id, LocalDate date){
+        this.name = name;
+        this.id = id;
+        this.date = date;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
