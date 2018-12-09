9a243c8806bb59c2a5c7ff89ee5f45b60b38b544
eyuell <45358912+eyuell@users.noreply.github.com>
2018-12-09 21:59:54 +0100

    Class for Planned Manpower

---
+++ ManpowerAllocation.java
@@ -0,0 +1,27 @@
+package MiniProject;
+
+import java.time.LocalDate;
+
+public class ManpowerAllocation {
+    private Manpower manpower;
+    private double workHours;
+    private LocalDate date;
+
+    ManpowerAllocation(Manpower manpower, double workHours, LocalDate date){
+        this.manpower = manpower;
+        this.workHours = workHours;
+        this.date = date;
+    }
+
+    public double getWorkHours() { return workHours; }
+
+    public Manpower getManpower() { return manpower; }
+
+    public LocalDate getDate() { return date; }
+
+    public void setManpower(Manpower manpower) { this.manpower = manpower; }
+
+    public void setWorkHours(double workHours) { this.workHours = workHours; }
+
+    public void setDate(LocalDate date) { this.date = date; }
+}
