d0a3a13e5610e48e7ecb8ad9097dd7eb288b434e
eyuell <45358912+eyuell@users.noreply.github.com>
2018-12-09 22:02:57 +0100

    Optimized for json IO

--- Project.java
+++ Project.java
@@ -1,4 +1,4 @@
-package ProjectScheduling;
+package MiniProject;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -6,22 +6,22 @@ import java.util.ArrayList;
 public class Project {
     private String name;
     private String projectID;
-    private ArrayList<Task> tasks;
-    //private ArrayList<Finance> finances;
-    //private ArrayList<Risk> risks;
-    private long duration;
     private LocalDate startDate;
     private LocalDate finishDate;
-    private double cost;
+    private long duration;
+    private ArrayList<Milestone> milestones;
+    private ArrayList<Task> tasks;
+    private ArrayList<TeamMember> teamMembers;
+    private ArrayList<Risk> risks;
 
     public Project (String name, String projectID, LocalDate startDate){
         this.name = name;
         this.projectID = projectID;
         this.startDate = startDate;
+        this.milestones = new ArrayList<>();
         this.tasks = new ArrayList<>();
-        //this.finances = new ArrayList<>();
-        //this.risks = new ArrayList<>();
-
+        this.teamMembers = new ArrayList<>();
+        this.risks = new ArrayList<>();
     }
 
     public String getName() {
@@ -38,27 +38,17 @@ public class Project {
 
     public LocalDate getFinishDate() { return finishDate; }
 
-    public void addTask(Task task){
-        this.tasks.add(task);
-    }
-
-    //public void addFinance(Finance finance){this.finances.add(finance);}
-
-    //public void addRisk(Risk risk){this.risks.add(risk);}
-
     public ArrayList<Task> getTasks() {
         return tasks;
     }
 
     public LocalDate getStartDate() { return startDate; }
 
-    public double getCost() {
-        return cost;
-    }
+    public ArrayList<TeamMember> getTeamMembers() { return teamMembers; }
 
-    //public ArrayList<Finance> getFinances() { return finances; }
+    public ArrayList<Risk> getRisks() { return risks; }
 
-    //public ArrayList<Risk> getRisks() { return risks; }
+    public ArrayList<Milestone> getMilestones() { return milestones; }
 
     public void setName(String name) {
         this.name = name;
@@ -75,10 +65,6 @@ public class Project {
     public void setStartDate(LocalDate startDate) { this.startDate = startDate; }
 
     public void setFinishDate(LocalDate finishDate) { this.finishDate = finishDate; }
-
-    public void setCost(double cost) {
-        this.cost = cost;
-    }
 
     public void setTasks(ArrayList<Task> tasks) {
         this.tasks = tasks;
