70e309712b3151574a0ec3527540c934d5d8649b
eyuell <45358912+eyuell@users.noreply.github.com>
2018-12-09 22:18:19 +0100

    almost same file

--- RiskEvaluator.java
+++ RiskEvaluator.java
@@ -10,7 +10,37 @@ public class RiskEvaluator {
     public double probability (double probability){
         double leastValue = 0.0;
         double maxValue = 10.0;
-        while (probability < leastValue || probability > maxValue){
+        while (probabipackage MiniProject;
+
+        public class RiskEvaluator {
+
+            public String name (String name){
+                name = name.toUpperCase();
+                return name;
+            }
+
+            public double probability (double probability){
+                double leastValue = 0.0;
+                double maxValue = 10.0;
+                while (probability < leastValue || probability > maxValue){
+                    System.out.println("Enter a value between 1 and 10");
+                    probability = new KeyboardInput().positiveDouble();
+                    probability = new MiniProject.RiskEvaluator().probability(probability);
+                }
+                return probability;
+            }
+
+            public double impact (double impact){
+                double leastValue = 0.0;
+                double maxValue = 10.0;
+                while (impact < leastValue || impact > maxValue){
+                    System.out.println("Enter a value between 1 and 10.");
+                    impact = new KeyboardInput().positiveDouble();
+                    impact = new MiniProject.RiskEvaluator().impact(impact);
+                }
+                return  impact;
+            }
+        }lity < leastValue || probability > maxValue){
             System.out.println("Enter a value between 1 and 10");
             probability = new KeyboardInput().positiveDouble();
             probability = new MiniProject.RiskEvaluator().probability(probability);
