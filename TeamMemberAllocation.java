e01b0d0d3248a66e83c7405d7c9186eb3bf3e3ec
eyuell <45358912+eyuell@users.noreply.github.com>
2018-12-09 22:31:20 +0100

    Actual teams allocation Class

---
+++ TeamMemberAllocation.java
@@ -0,0 +1,38 @@
+package MiniProject;
+
+import java.time.LocalDate;
+
+public class TeamMemberAllocation {
+    private TeamMember teamMember;
+    private double workHours;
+    private LocalDate date;
+    TeamMemberAllocation(TeamMember teamMember, double workHours, LocalDate date){
+        this.teamMember = teamMember;
+        this.workHours = workHours;
+        this.date = date;
+    }
+
+    public double getWorkHours() {
+        return workHours;
+    }
+
+    public TeamMember getTeamMember() {
+        return teamMember;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setTeamMember(TeamMember teamMember) {
+        this.teamMember = teamMember;
+    }
+
+    public void setWorkHours(double workHours) {
+        this.workHours = workHours;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+}
