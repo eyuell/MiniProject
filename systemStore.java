47e73b6fff13738392895d376994bb0e349eaf28
armin2118 <43929673+armin2118@users.noreply.github.com>
2018-12-09 19:04:28 +0100

    Update systemStore.java
    New methods for printing information per ID

--- systemStore.java
+++ systemStore.java
@@ -4,34 +4,47 @@ import java.util.List;
 public class systemStore {
     private List<Finance> finances;
 
-    public systemStore(){
+    public systemStore() {
         this.finances = new ArrayList<>();
     }
 
-    public Finance registerScheduleVariance(double budget, double earnedValue, double projectDuration, double daysPassed, int projectId) {
-
-        Finance newSV = new scheduleVariance(budget, earnedValue, projectDuration, daysPassed);
+//Add new SV
+    public Finance registerScheduleVariance(double budget, double earnedValue, double projectDuration, double daysPassed, String projectID) {
+        Finance newSV = new scheduleVariance(budget, earnedValue, projectDuration, daysPassed, projectID);
         finances.add(newSV);
         return newSV;
     }
-    public Finance registerCostVariance(double budget, double earnedValue, double projectDuration, double daysPassed, double actualCost){
-        Finance newCV=new costVariance(budget,earnedValue ,actualCost ,projectDuration ,daysPassed );
+
+//Add new CV
+    public Finance registerCostVariance(double budget, double earnedValue, double projectDuration, double daysPassed, double actualCost, String projectID) {
+        Finance newCV = new costVariance(budget, earnedValue, actualCost, projectDuration, daysPassed, projectID);
         finances.add(newCV);
         return newCV;
     }
 
+//Print a INT of how many Objects are stored in array
     public int sizeOfArray() {
         return finances.size();
     }
 
+//Print all stored values for all projects.
     public String printAllFinances() {
         for (int i = 0; i < finances.size(); i++) {
             if (finances.get(i) != null)
                 System.out.println(finances.get(i).toString());
         }
         return null;
-
-
     }
-
+//Print all values for a specific project ID
+    public Finance printAllFinancesByID(String projectID) {
+        for (int i = 0; i < finances.size(); i++) {
+            if (finances.get(i) != null && finances.get(i).getProjectID().equals(projectID)) {
+                System.out.println(finances.get(i).toString());
+            }
+        }
+        return null;
+    }
 }
+
+
+
