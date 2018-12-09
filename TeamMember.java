1c4b3bc8c6ac51a5040beecbc35846d3199b340f
eyuell <45358912+eyuell@users.noreply.github.com>
2018-12-09 22:30:23 +0100

    Optimized for inheritance

--- TeamMember.java
+++ TeamMember.java
@@ -1,22 +1,34 @@
 package MiniProject;
 
-
-import java.time.LocalDate;
-
 public class TeamMember extends Manpower {
     private String name;
+    private String id;
+    private double hourlyRate;
 
-    public TeamMember(String name, String qualification, double hoursWorked, Task task, LocalDate date){
-        super(qualification, hoursWorked, task, date);
+    public TeamMember(String name, String id, String qualification){
+        super(qualification);
         this.name = name;
+        this.id = id;
+        setHourlyRate(255.0);
     }
 
     public String getName() {
         return name;
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public double getHourlyRate() { return hourlyRate; }
+
     public void setName(String name) {
         this.name = name;
     }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setHourlyRate(double hourlyRate) { this.hourlyRate = hourlyRate; }
 }
