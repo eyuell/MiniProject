cd9a70e5cc61439c65e8f631914037263e4507ff
eyuell <45358912+eyuell@users.noreply.github.com>
2018-12-17 18:08:47 +0100

    input handler change

--- RiskEvaluator.java
+++ RiskEvaluator.java
@@ -1,8 +1,6 @@
 package PersonalCodeTraining.MiniProject;
 
-import PersonalCodeTraining.InputHandler;
-
-public class RiskEvaluator {
+public class RiskEvaluator{
 
     public String name (String name){
         name = name.toUpperCase();
@@ -14,10 +12,9 @@ public class RiskEvaluator {
         double maxValue = 10.0;
         while (probability < leastValue || probability > maxValue){
             System.out.println("Enter a value between 1 and 10");
-            probability = new InputHandler().Double();
-            probability = new PersonalCodeTraining.ProjectManagement.RiskEvaluator().probability(probability);
+            probability = new KeyboardInput().positiveDouble();
+            probability = new RiskEvaluator().probability(probability);
         }
-
         return probability;
     }
 
@@ -26,8 +23,8 @@ public class RiskEvaluator {
         double maxValue = 10.0;
         while (impact < leastValue || impact > maxValue){
             System.out.println("Enter a value between 1 and 10.");
-            impact = new InputHandler().Double();
-            impact = new PersonalCodeTraining.ProjectManagement.RiskEvaluator().impact(impact);
+            impact = new KeyboardInput().positiveDouble();
+            impact = new RiskEvaluator().impact(impact);
         }
         return  impact;
     }
